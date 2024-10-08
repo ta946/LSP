7b9728a4b12df772a7dc6a0d607e37653adcd3e6
tai <tai@gameface.ai>
2024-10-08 16:07:07 +1100

    ﻿get hover content

--- boot.py
+++ boot.py
@@ -56,6 +56,7 @@ from .plugin.hierarchy import LspCallHierarchyCommand
 from .plugin.hierarchy import LspHierarchyToggleCommand
 from .plugin.hierarchy import LspTypeHierarchyCommand
 from .plugin.hover import LspCopyTextCommand
+from .plugin.hover import LspGetHoverContentCommand
 from .plugin.hover import LspHoverCommand
 from .plugin.hover import LspToggleHoverPopupsCommand
 from .plugin.inlay_hint import LspInlayHintClickCommand
@@ -122,6 +123,7 @@ __all__ = (
     "LspFormatCommand",
     "LspFormatDocumentCommand",
     "LspFormatDocumentRangeCommand",
+    "LspGetHoverContentCommand",
     "LspGotoDiagnosticCommand",
     "LspHierarchyToggleCommand",
     "LspHoverCommand",
